1e3394d1bd51971605898e03ee4c7be028de1597
tenglor <isdigni@gmail.com>
2023-03-25 11:13:13 +0300

    ghost scripts++

---
+++ Assets/Code/Services/StateMachine/DetectPlayerDecision.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Services.StateMachine
+{
+    [CreateAssetMenu(menuName = "FSM/Decisions/PlayerInView")]
+    class DetectPlayerDecision : Decision
+    {
+        public float detectRadius = 1f;
+        
+        public override bool Decide(BaseStateMachine state)
+        {
+            Vector2 playerPos = GameObject.FindGameObjectWithTag("player").transform.position;
+            Vector2 objPosition = state.transform.position;
+            
+            return Vector2.Distance(playerPos, objPosition) < 1f;
+        }
+    }
+}
